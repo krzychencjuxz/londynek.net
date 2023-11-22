4c6d3789c7c408898d7ce5d4c4f34a063174e435
Krzysiek <krzysztof.kwiczala@interia.pl>
2023-11-22 12:44:43 +0100

    sprawdzenie co jest nie tak

---
+++ src/components/AddNews.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddNews() {
+  return (
+    <div>
+      <h2>Add News</h2>
+      {/* Form for adding news */}
+    </div>
+  );
+}
+
+export default AddNews;
---
+++ src/components/NewsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NewsList() {
+  return (
+    <div>
+      <h2>News List</h2>
+      {/* Placeholder for news items */}
+    </div>
+  );
+}
+
+export default NewsList;
